ad40162572c07d52495d3f0facdb18ac1e4fe1ce
simonhodson <simonhodson72@gmail.com>
2022-09-17 21:37:23 +0100

    stable: initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000');
+});
